7b39d45939f31971c440aae4cb3d87b4e1257d41
Anton Delprado <anton.delprado@gmail.com>
2012-05-31 10:55:45 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wolf::Application.config.secret_token = '7271207882fa702ff6865d1172a7128114573a4464eb48f896472670538cb1467f43b4b9f0f20b6424f5579b3337c8e68fec91c8dc8346684fdb9069acd550fc'
